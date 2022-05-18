abc48c0beeae203d30911391b3fca058c96c8f6b
Evan Liu <liu928624@126.com>
2022-05-18 21:58:36 +0800

    bean annotation

---
+++ official-context/src/main/java/com/evan/thinking/spring/official/context/bean/annotation/AutowiredPeppaPig.java
@@ -0,0 +1,50 @@
+package com.evan.thinking.spring.official.context.bean.annotation;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.evan.thinking.spring.official.context.model.PeppaPig;
+
+import lombok.ToString;
+
+/**
+ * @desc: Autowired 注解使用方式
+ * @author: evan
+ * @date: 2022/5/18
+ */
+@ToString
+public class AutowiredPeppaPig {
+
+    private PeppaPig constructPeppaPig;
+
+    private PeppaPig setterPeppaPig;
+
+    private PeppaPig methodPeppaPig;
+
+    @Autowired
+    private PeppaPig filedPeppaPig;
+
+    @Autowired
+    public PeppaPig[] peppaPigArr;
+
+    @Autowired
+    public Map<String, PeppaPig> peppaPigMap;
+
+    @Autowired
+    public Map<String, Object> allObject;
+
+    @Autowired
+    public AutowiredPeppaPig(PeppaPig constructPeppaPig) {
+        this.constructPeppaPig = constructPeppaPig;
+    }
+
+    @Autowired
+    public void setSetterPeppaPig(PeppaPig setterPeppaPig) {
+        this.setterPeppaPig = setterPeppaPig;
+    }
+
+    public void methodPeppaPig(PeppaPig methodPeppaPig) {
+        this.methodPeppaPig = methodPeppaPig;
+    }
+}
